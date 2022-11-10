688e9792058734da81b9fb2ea9505a036d5f0460
torebias <ebias.vitor@ime.eb.br>
2022-11-10 15:14:15 -0300

    desenvolivimento front-end

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="Templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 
 # Autenticação
 @app.post("/auth")
@@ -38,3 +45,16 @@ def read_item():
 @app.get("/vehicles/query")
 def read_item():
     pass
+
+@app.get("/login", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/sigin/vehicles", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("cadastro_vtr.html", {"request": request})
+
+@app.get("/request/vehicles", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("pedido_vtr.html", {"request": request})
+
